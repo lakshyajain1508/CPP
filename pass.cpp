2af37cb961a326047d8fea75d14160974c6b7b66
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-15 20:14:30 +0530

    Update pass.cpp

--- pass.cpp
+++ pass.cpp
@@ -1,3 +1,4 @@
+// enter the password
 // Password Checker Example :
 
 #include <iostream>
@@ -47,89 +48,93 @@ int main()
             cout << "Try next year";
         }
     }
-    else if (pass2 != pass)
-
-    {
-        string pass3;
-        cout << "wrong password ... try 2 more time out of 2\n";
-        cin >> pass3;
-        int ans;
-        cout << "1. Who invented Java Programming?\n";
-        cout << "1. ) Guido van Rossum\n";
-        cout << "2. ) James Gosling\n";
-        cout << "3. ) Dennis Ritchie\n";
-        cout << "4. ) Bjarne Stroustrup\n";
-        cout << "Select answer : ";
-        cin >> ans;
-
-        if (ans == 2)
-        {
-            cout << "Next one :- \n";
-            int ans;
-            cout << "2. Which component is used to compile, debug and execute the java programs?\n";
-            cout << "1. ) JRE\n";
-            cout << "2. ) JIT\n";
-            cout << "3. ) JDK\n";
-            cout << "4. ) JVM\n";
-            cout << "Select answer : ";
-            cin >> ans;
-            if (ans == 2)
-            {
-                cout << "Hurray!!";
-            }
-            else
-            {
-                cout << "Wrong,come next year";
-            }
-        }
-        else
-        {
-            cout << "Try next year";
-        }
-    }
-
-    else if (pass2 != pass)
-    {
-        string pass4;
-        cout << "wrong password ... try 2 more time out of 2\n";
-        cin >> pass4;
-        int ans;
-        cout << "1. Who invented Java Programming?\n";
-        cout << "1. ) Guido van Rossum\n";
-        cout << "2. ) James Gosling\n";
-        cout << "3. ) Dennis Ritchie\n";
-        cout << "4. ) Bjarne Stroustrup\n";
-        cout << "Select answer : ";
-        cin >> ans;
-
-        if (ans == 2)
-        {
-            cout << "Next one :- \n";
-            int ans;
-            cout << "2. Which component is used to compile, debug and execute the java programs?\n";
-            cout << "1. ) JRE\n";
-            cout << "2. ) JIT\n";
-            cout << "3. ) JDK\n";
-            cout << "4. ) JVM\n";
-            cout << "Select answer : ";
-            cin >> ans;
-            if (ans == 2)
-            {
-                cout << "Hurray!!";
-            }
-            else
-            {
-                cout << "Wrong,come next year";
-            }
-        }
-        else
-        {
-            cout << "Try next year";
-        }
-    }
     else
     {
-        cout << " wrong password ... try 0 more time 0";
+        string pass3;
+        cout << "wrong password ... try 2 more time out of 2 - ";
+        cin >> pass3;
+        if (pass3 == pass)
+        {
+            int ans;
+            cout << "1. Who invented Java Programming?\n";
+            cout << "1. ) Guido van Rossum\n";
+            cout << "2. ) James Gosling\n";
+            cout << "3. ) Dennis Ritchie\n";
+            cout << "4. ) Bjarne Stroustrup\n";
+            cout << "Select answer : ";
+            cin >> ans;
+
+            if (ans == 2)
+            {
+                cout << "Next one :- \n";
+                int ans;
+                cout << "2. Which component is used to compile, debug and execute the java programs?\n";
+                cout << "1. ) JRE\n";
+                cout << "2. ) JIT\n";
+                cout << "3. ) JDK\n";
+                cout << "4. ) JVM\n";
+                cout << "Select answer : ";
+                cin >> ans;
+                if (ans == 2)
+                {
+                    cout << "Hurray!!";
+                }
+                else
+                {
+                    cout << "Wrong,come next year";
+                }
+            }
+            else
+            {
+                cout << "Try next year";
+            }
+        }
+        else
+        {
+            string pass4;
+            cout << "wrong password ... try 1 more time out of 1 - ";
+            cin >> pass4;
+            if (pass4 == pass)
+            {
+                int ans;
+                cout << "1. Who invented Java Programming?\n";
+                cout << "1. ) Guido van Rossum\n";
+                cout << "2. ) James Gosling\n";
+                cout << "3. ) Dennis Ritchie\n";
+                cout << "4. ) Bjarne Stroustrup\n";
+                cout << "Select answer : ";
+                cin >> ans;
+
+                if (ans == 2)
+                {
+                    cout << "Next one :- \n";
+                    int ans;
+                    cout << "2. Which component is used to compile, debug and execute the java programs?\n";
+                    cout << "1. ) JRE\n";
+                    cout << "2. ) JIT\n";
+                    cout << "3. ) JDK\n";
+                    cout << "4. ) JVM\n";
+                    cout << "Select answer : ";
+                    cin >> ans;
+                    if (ans == 2)
+                    {
+                        cout << "Hurray!!";
+                    }
+                    else
+                    {
+                        cout << "Wrong,come next year";
+                    }
+                }
+                else
+                {
+                    cout << "Try next year";
+                }
+            }
+            else
+            {
+                cout << " wrong password ... try 0 more time 0";
+            }
+        }
     }
     return 0;
 }
