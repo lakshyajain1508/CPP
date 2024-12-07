988aeb277e37730991fc64f73dfd06082508b084
Lakshya Jain <lakshyajain1508@gmail.com>
2024-12-07 18:50:56 +0530

    Create stringarray.cpp

---
+++ stringarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout<<"Enter the size of array = ";
+    cin>>size;
+    
+    string data[size];
+    
+    for(int i = 0; i < size; i++){
+        cout<<"Enter the Elements "<<(i+1)<<" = ";
+        cin>>data[i];
+    }
+    
+    for(int i = 0; i < size; i++){
+        cout<<(i+1)<<" = "<<data[i]<<endl;    
+    }
+    
+    
+}
