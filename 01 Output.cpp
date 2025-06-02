fb95e9c65175a1308cdd8100f5203979543134e2
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-02 13:02:35 +0530

    topic how to get output & types of comment

---
+++ 01 Output.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// print hello world
+int main(){
+    cout<<"Hello World!!\n";
+    cout<<"123\n";
+}
---
+++ 02 Comments.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// How to write comments in C++
+
+// Single line comment
+
+/* 
+    Multi-line comment
+   This is a multi-line comment.
+*/
+
+int main() {
+
+    // This is a single line comment explaining the next line of code
+    cout << "This is a single line comment example." << endl;
+
+    /*
+        This is a multi-line comment.
+        It can span multiple lines.
+    */
+    cout << "This is a multi-line comment example." << endl;
+
+    return 0; // End of the program
+}
