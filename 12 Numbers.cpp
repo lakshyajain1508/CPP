10adcdc783a032a8384d09fc455e03ddf73cfd84
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-08 22:54:33 +0530

    topic number cmath library

---
+++ 12 Numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+// sqrt(625)
+// pow(2, 3)
+// abs(-7)
+
+using namespace std;
+
+int main(){
+
+    cout<<"SquareRoot of 625 = "<<sqrt(625)<<endl;
+    cout<<"2 to power of 3 = "<<pow(2, 3)<<endl;
+    cout<<"Abosulte Value = "<<abs(-7)<<endl;
+
+    return 0;
+}
