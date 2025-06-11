77d8a2448ee0da072b7e5ff9467650d5ea0b7c57
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-11 21:39:46 +0530

    Topic Multiple Inheritance

--- 23 MultipleInheritance.cpp
+++ 23 MultipleInheritance.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
     // Multiple Inheritance - Multiple inheritance involves a class deriving from more than one base class. This can lead to complex class hierarchies.
 
-class Grandfather{
+class Teacher{
     public:
-        void grand(){
-            cout << "I am Grandfather." << endl;
+        void teach(){
+            cout << "I am Teacher." << endl;
         }
 };
 
-class Father{
+class Student{
     public:
-        void father(){
-            cout << "I am Father." << endl;
+        void student(){
+            cout << "I am Student." << endl;
         }
 };
 
-class Son : public Grandfather , public Father{
+class Staff : public Teacher , public Student{
     public:
-        void son(){
-            cout << "I am Son." << endl;
+        void staff(){
+            cout << "I am Staff." << endl;
         }
 };
 
 int main(){
-    Son fam;
-    fam.grand();
-    fam.father();
-    fam.son();
+    Staff school;
+    school.teach();
+    school.student();
+    school.staff();
     return 0;
 }
